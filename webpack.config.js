87da815b9022d5b839974cd862740725d57e1cf9
davnalbandyan <davnalbandyan@gmail.com>
2023-08-20 13:06:23 +0400

    so change the modal and forms,fix the problem,add the babel,install the packages & plagins

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const path = require('node:path');
 
 module.exports = {
-  mode: 'development',//development,prodaction
+  mode: 'production',//development,prodaction
   entry: './src/JS/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'all.js',
   },
   watch: true,
-  devtool:"source-map"
+  devtool:"source-map",
+   module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
+  
+  
 };
